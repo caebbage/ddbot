4d2605336a8c946c384d479ec509c6e3ee21aa58
cae <caebbage@users.noreply.github.com>
2025-01-21 17:59:28 -0600

    my bad

--- cmd/path.js
+++ cmd/path.js
@@ -47,7 +47,7 @@ exports.run = async (client, message, inputs, comment) => { // eslint-disable-li
             x.subpaths.split(",").map(x => x.toLowerCase().trim()).includes("error"));
 
           embeds = [{
-            description: errorPath[0] || `Subpath not found... double check if you typoed, and ping teru if you didn't.`,
+            description: errorPath[0].value || `Subpath not found... double check if you typoed, and ping teru if you didn't.`,
             color: client.config.get('embed_color')
           }]
 
